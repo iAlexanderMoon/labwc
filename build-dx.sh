0cf205680996b317a05f3d84102c9cc9bda42a94
Ian Moon <moon@openlever.com>
2024-11-03 20:28:46 -0700

    Try building dx by creating a seperate build-dx.sh script

---
+++ build-dx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+rpm-ostree install containerd.io
+rpm-ostree install docker-ce
+rpm-ostree install docker-ce-cli
+rpm-ostree install docker-compose-plugin
+
+# this would install a package from rpmfusion
+# rpm-ostree install vlc
+
+#### Example for enabling a System Unit File
+
+#systemctl enable podman.socket
+
