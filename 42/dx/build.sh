7ff1d5da69ce4a0bec01dae409a01de415952924
Ian Moon <moon@openlever.com>
2025-03-30 12:04:46 -0600

    fixed typi in cosmic

--- 42/dx/build.sh
+++ 42/dx/build.sh
@@ -39,7 +39,7 @@ dnf5 -y install code
 ## Let also add these cosmic tools while we are at it to try them out.
 dnf5 -y cosmic-term
 dnf5 -y cosmic-player
-dnf5 -y comsic-store
+dnf5 -y cosmic-store
 dnf5 -y cosmic-files
 dnf5 -y cosmic-launcher
 dnf5 -y cosmic-edit
