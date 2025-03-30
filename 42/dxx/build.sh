54460d0ebda4277d8b89681a1e9ac8f99e3dd47b
Ian Moon <moon@openlever.com>
2025-03-30 12:53:58 -0600

    missing install instruction on dnf5 command.

--- 42/dxx/build.sh
+++ 42/dxx/build.sh
@@ -7,9 +7,9 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 
 ## Let also add these cosmic tools while we are at it to try them out.
-dnf5 -y cosmic-term
-dnf5 -y cosmic-player
-dnf5 -y cosmic-store
-dnf5 -y cosmic-files
-dnf5 -y cosmic-launcher
-dnf5 -y cosmic-edit
+dnf5 -y install cosmic-term
+dnf5 -y install cosmic-player
+dnf5 -y install cosmic-store
+dnf5 -y install cosmic-files
+dnf5 -y install cosmic-launcher
+dnf5 -y install cosmic-edit
