33f8097b413e82c43faf76b9435e36f794e1f662
Ian Moon <moon@openlever.com>
2025-03-30 12:26:16 -0600

    removed cosmic apps for now from dx. adding chayang and wlopm for managing dispaly outout with wayidle

--- 42/dx/build.sh
+++ 42/dx/build.sh
@@ -37,12 +37,12 @@ dnf5 -y install toolbox
 dnf5 -y install code
 
 ## Let also add these cosmic tools while we are at it to try them out.
-dnf5 -y cosmic-term
-dnf5 -y cosmic-player
-dnf5 -y cosmic-store
-dnf5 -y cosmic-files
-dnf5 -y cosmic-launcher
-dnf5 -y cosmic-edit
+#dnf5 -y cosmic-term
+#dnf5 -y cosmic-player
+#dnf5 -y cosmic-store
+#dnf5 -y cosmic-files
+#dnf5 -y cosmic-launcher
+#dnf5 -y cosmic-edit
 
 #### Example for enabling a System Unit File
 
