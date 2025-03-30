4275230bbd616d2c50d49c9eaa9f2d295a7e7309
Ian Moon <moon@openlever.com>
2025-03-30 14:54:48 -0600

    removed cosmic apps... adding alacritty

--- 42/dxx/build.sh
+++ 42/dxx/build.sh
@@ -7,9 +7,12 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 
 ## Let also add these cosmic tools while we are at it to try them out.
-dnf5 -y install cosmic-term
-dnf5 -y install cosmic-player
-dnf5 -y install cosmic-store
-dnf5 -y install cosmic-files
-dnf5 -y install cosmic-launcher
-dnf5 -y install cosmic-edit
+#dnf5 -y install cosmic-term
+#dnf5 -y install cosmic-player
+#dnf5 -y install cosmic-store
+#dnf5 -y install cosmic-files
+#dnf5 -y install cosmic-launcher
+#dnf5 -y install cosmic-edit
+
+## Lets give alacritty a go... whatelse?
+dnf5 -y install alacritty
